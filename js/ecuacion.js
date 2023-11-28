511c499f11c0137c30ec2ce36525dd1abfaad562
Alexis Rodriguez <ale_gr@live.com>
2023-11-28 17:54:48 -0300

    a medida que agrega va calculando
    a medida que agrega va calculando, falta solucionar cuando se pone el primer insumo

--- js/ecuacion.js
+++ js/ecuacion.js
@@ -1,10 +1,23 @@
-function CalcularEcuacion(){
+function CalcularEcuacion() {
+    let filas = document.querySelectorAll('#camposExtras .row');
+    let sumaResultados = 0;
 
-    let codos = document.getElementById("codos").value;
-    let IncPas = document.getElementById("IncPas").value;
-    let DiamCa = document.getElementById("DiamCa").value;
+    let codos1 = document.getElementById("codos").value;
+        let IncPas1 = document.getElementById("IncPas").value;
+        let DiamCa1 = document.getElementById("DiamCa").value;
+        let Resultado1 = codos1 * IncPas1 * DiamCa1;
 
-    let Resultado = codos*IncPas*DiamCa;
-    document.getElementById("ResultadoEcua").innerText = `Resultado: ${Number(Resultado.toFixed(2))} m³/h`;
+    filas.forEach((fila) => {
+       
+        let codos = fila.querySelector('input[placeholder="Cantidad de codos (un)"]').value;
+        let IncPas = fila.querySelector('input[placeholder="Inclinación de pasaje"]').value;
+        let DiamCa = fila.querySelector('input[placeholder="Diámetro cañería (Metros)"]').value;
 
+        
+
+        let Resultado = codos * IncPas * DiamCa;
+        sumaResultados += parseFloat(Resultado || 0); // Asegúrate de sumar un valor numérico
+    });
+
+    document.getElementById("ResultadoEcua").innerText = `Suma total: ${Number(sumaResultados+Resultado1.toFixed(2))} m³/h`;
 }
