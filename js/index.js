3bcfe4aafd93a88287c784c0742992278abe0d71
Alexis Rodriguez <ale_gr@live.com>
2023-12-03 19:13:02 -0300

    reiniciar valor Kcal
    se agrego la funcion reiniciar kcal

--- js/index.js
+++ js/index.js
@@ -3,8 +3,12 @@ function PasarKcal(){
     let RJoule = Joule/9300;
 
     document.getElementById("ResultadoKcal").innerText = `Resultado: ${Number(RJoule.toFixed(2))} m³/h`;
-    
-    
+        
 }
 
+function ReiniciarAppKcal() {
+    document.getElementById("Kcal").value = ""; // Reinicia el valor de LongMetro
+    document.getElementById("ResultadoKcal").innerText = `Resultado:`; // Reinicia el valor de CantMedidores
+
+}
 
