a3582f88fe6fc752d62809e8eed1aeb7279e5bd3
Alexis Rodriguez <ale_gr@live.com>
2023-12-03 21:07:15 -0300

    se agego la funcion mostrar ecuacion
    se agrego el boton y la tabla a mostrar, falta que muestre todas los insumos que agrego

--- js/ecuacion.js
+++ js/ecuacion.js
@@ -38,3 +38,42 @@ function CalcularEcuacion() {
         total = Resultado1 + sumaResultados;
     document.getElementById("ResultadoEcua").innerText = `Resultado: ${Number(total.toFixed(2))} m³/h`;
 }
+
+
+
+var w = document.getElementById("mostrarTablaEcua");
+const botonMostrarTablaEcua = document.querySelector('.btn.btn-primary');
+  function mostrarTablaEcuacion() {
+    
+
+    const insumo = document.querySelector('.form-select').value;
+    const cantidadInsumo = document.getElementById('codos').value;
+    const caidaPresion = document.getElementById('IncPas').value;
+    const diametroCaneria = document.getElementById('DiamCa').value;
+    const resultadoEcua = document.getElementById("ResultadoEcua").innerText;
+
+    // Mostrar los valores en la tabla oculta
+    document.getElementById('filaDatosEcua').innerHTML = `
+      <td>${insumo}</td>
+      <td>${cantidadInsumo}</td>
+      <td>${caidaPresion}</td>
+      <td>${diametroCaneria}</td>
+      <td>${resultadoEcua}</td>
+    `;
+
+    w.style.display = "block";
+  }
+  function ocultarTablaEcua() {
+    // Ocultar la tabla al hacer clic en el botón "Close"
+    w.style.display = 'none';
+    
+}
+
+document.addEventListener('mouseup', function(e) {
+    if (w.style.display === 'block' && !w.contains(e.target) && e.target !== botonMostrarTablaEcua) {
+        ocultarTablaEcua();
+    }
+});
+
+
+                
